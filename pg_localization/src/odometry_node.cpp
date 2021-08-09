3bc100279f12454e27220747153d359839f09a9d
Wahyu Mahardika <wmhrdk@gmail.com>
2021-08-09 22:44:56 +0800

    Added license statements

--- pg_localization/src/odometry_node.cpp
+++ pg_localization/src/odometry_node.cpp
@@ -1,4 +1,27 @@
-/* INCLUDE HEADERS */
+// Copyright (c) 2021 EPROM PNUP
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom
+// the Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+//
+// Author: Wahyu Mahardika
+
+
+// INCLUDE HEADERS //
 #include <ros/ros.h>
 #include <tf/transform_broadcaster.h>
 #include <nav_msgs/Odometry.h>
@@ -6,18 +29,18 @@
 #include <std_msgs/Int16MultiArray.h>
 #include <cmath>
 
-/* PARAMETERS */
+// PARAMETERS //
 #define BASE 0.2
 #define WHEEL_RADIUS 0.05
 #define PULSE_PER_METER 426 
 #define PI 3.141592
 
-/* WHEEL INDEX */
+// WHEEL INDEX //
 #define WHEEL_A 0
 #define WHEEL_B 1
 #define WHEEL_C 2
 
-/* Odometry class definition */
+// ODOMETRY CLASS DEFINITION //
 class Odometry {
 
 	private:
@@ -176,7 +199,7 @@ class Odometry {
 	}
 };
 
-/* MAIN FUNCTION */
+// MAIN FUNCTION //
 int main(int argc, char** argv) {
 	
 	ros::init(argc, argv, "odometry_pub");
