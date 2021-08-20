575a3626e1f433fa3b8708a191acb50117bcc801
Wahyu Mahardika <wmhrdk@gmail.com>
2021-08-20 11:44:57 +0800

    Refactored wheel velocity calculations

--- pg_control/scripts/base_controller.py
+++ pg_control/scripts/base_controller.py
@@ -22,7 +22,7 @@
 #
 # Author: Wahyu Mahardika
 
-# IMPORT HEADERS
+import numpy as np
 import rospy
 from geometry_msgs.msg import Twist
 from pg_msgs.msg import MotorCommand
@@ -36,43 +36,49 @@ class MotorDriver:
 		rospy.init_node('base_controller')
 
 		self._last_received = rospy.get_time()
-		self._timeout = rospy.get_param('~timeout', 2)
-		self._rate = rospy.get_param('~rate', 10)
-		self._max_speed = rospy.get_param('~max_speed', 39.0)
+		self._wheel_radius = rospy.get_param('wheel_radius')
+		self._timeout = rospy.get_param('/base_controller/timeout')
+		self._rate = rospy.get_param('/base_controller/rate')
 
-		self._motor_1_speed = 0
-		self._motor_2_speed = 0
-		self._motor_3_speed = 0
+		self._wheel_velocity = [0.0, 0.0, 0.0]
+		self._body_twist = [0.0, 0.0, 0.0]
 
-		self._motors_speed = MotorCommand() 
+		self._wheel_velocity_msg = MotorCommand() 
+		self._wheel_velocity_msg.data = [0.0, 0.0, 0.0]
 
 		rospy.Subscriber('cmd_vel', Twist, self._velocity_callback)
 
-		self.motor_speed_pub = rospy.Publisher('motor_pwm', MotorCommand, queue_size=10)
+		self.motor_speed_pub = rospy.Publisher(
+			'wheel_refrence_velocity', 
+			MotorCommand, 
+			queue_size=10)
 
 	# Velocity callback function
 	def _velocity_callback(self, message):
 		self._last_received = rospy.get_time()
 
-		linear_x = message.linear.x
-		linear_y = message.linear.y
-		angular_z = message.angular.z
+		self._body_twist[0] = message.linear.x
+		self._body_twist[1] = message.linear.y
+		self._body_twist[2] = message.angular.z
 
-		self._motor_1_speed = (17.320508075688*linear_x)+(10*linear_y)+(4*angular_z)
-		self._motor_2_speed = (-17.320508075688*linear_x)+(10*linear_y)+(4*angular_z)
-		self._motor_3_speed = (0.0*linear_x)+(-20*linear_y)+(4*angular_z)
+		H = [
+			[np.sin(np.pi/3), 1/2, 0.2],
+			[-np.sin(np.pi/3), 1/2, 0.2],
+			[0, -1, 0.2]
+			]
 
-		self._motor_1_speed = (self._motor_1_speed/self._max_speed) * 255 
-		self._motor_2_speed = (self._motor_2_speed/self._max_speed) * 255
-		self._motor_3_speed = (self._motor_3_speed/self._max_speed) * 255
+		for (i in range(H)):
+			for (j in range(i):
+				j = j / self._wheel_radius
 
-		self._motor_1_speed = int(self._motor_1_speed)
-		self._motor_2_speed = int(self._motor_2_speed)
-		self._motor_3_speed = int(self._motor_3_speed)
+		for (i in range(len(self._wheel_velocity))):
+			for (j in range(len(self._body_twist))):
+				self._wheel_velocity[i] += H[i][j] * self._body_twist[j]
 
-		self._motors_speed.data = [self._motor_1_speed, self._motor_2_speed, self._motor_3_speed]
+		for (i in range(len(self._wheel_velocity_msg.data))):
+			self._wheel_velocity_msg.data[i] = self._wheel_velocity[i]
 
-		rospy.loginfo([self._motor_1_speed, self._motor_2_speed, self._motor_3_speed])
+		rospy.loginfo(self._wheel_velocity)
 
 	# Ros run function
 	def run(self):
@@ -95,5 +101,4 @@ def main():
 
 if (__name__ == '__main__'):
 	main()
-
 
