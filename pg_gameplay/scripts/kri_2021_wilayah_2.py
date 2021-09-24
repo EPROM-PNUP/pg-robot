0275c486ce122c43c94489cc5ade9c424ed8d48e
Wahyu Mahardika <wmhrdk@gmail.com>
2021-09-24 16:27:56 +0800

    Updated kri_2021_wilayah_2.py

--- pg_gameplay/scripts/kri_2021_wilayah_2.py
+++ pg_gameplay/scripts/kri_2021_wilayah_2.py
@@ -3,6 +3,7 @@
 import rospy
 
 from geometry_msgs.msg import Twist
+from pg_msgs.srv import KickBall
 
 on_ball = False
 
@@ -40,24 +41,54 @@ def receive_ball():
     while not on_ball:
         rospy.loginfo("Waiting for ball ...")
 
+def pass_ball():
+    rospy.wait_for_service("kick")
+    try:
+        kick_ball = rospy.ServiceProxy("kick", KickBall)
+        res = kick_ball(True)
+    except rospy.ServiceException as e:
+        rospy.logerror(e)
+
+def mission_1():
+    vel_msg.linear.x = 0.0
+    vel_msg.linear.y = 5.0
+    vel_msg.linear.z = 0.0
+
+    vel_msg.angular.x = 0.0
+    vel_msg.angular.y = 0.0
+    vel_msg.angular.z = 0.0
+
+    move(vel_msg, rospy.Duration(1.5))
+
+    receive_ball()
+
+    vel_msg.linear.x = 0.0
+    vel_msg.linear.y = 0.0
+    vel_msg.linear.z = 0.0
+
+    vel_msg.angular.x = 0.0
+    vel_msg.angular.y = 0.0
+    vel_msg.angular.z = 4.0
+
+    move(vel_msg, rospy.Duration(1.5))
+
+    pass_ball()
+
+    # move()
+    # receive_ball()
+    # pass_ball()
+
 if __name__ == '__main__':
     try:
         rospy.init_node("kri_2021_node")
         
         while True:
-            vel_msg.linear.x = 0.0
-            vel_msg.linear.y = 4.0
-            vel_msg.linear.z = 0.0
+            print("--- KRI 2021 Wilayah II ---")
+            print("Misi: 1, 2, 3.")
 
-            vel_msg.angular.x = 0.0
-            vel_msg.angular.y = 0.0
-            vel_msg.angular.z = 0.0
+            mission = int(input("Pilih Misi: "))
 
-            move(vel_msg, rospy.Duration(1.5))
-
-            receive_ball()
-
-            pass_ball()
-            move()
-            receive_ball()
-            pass_ball()
+            if mission == 1:
+                mission_1()
+            elif mission == 2:
+                mission_2()
